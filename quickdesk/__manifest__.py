fb9c416f43c47c235fc6ea20d14fcfbde1a70607
SPEER0W-CELL <vansh.lalri@gmail.com>
2025-08-02 12:37:50 +0530

    Create __manifest__.py

---
+++ quickdesk/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'QuickDesk',
+    'version': '1.0',
+    'summary': 'Gamified project module for QuickDesk Hackathon',
+    'author': 'Vansh & GuildHub',
+    'category': 'Project',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/quickdesk_view.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
